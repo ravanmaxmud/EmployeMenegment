4c1bfd6214f1459d4a1c65593d4b627b6ca18555
ravanmaxmud <ravanim@code.edu.az>
2022-11-20 13:19:15 +0400

    Aded custom data annotation validation attribute  for FinCode

---
+++ EmpleyeMengment/EmpleyeMengment/Utilities/ValidFinCodeAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace EmpleyeMengment.Utilities
+{
+    public class ValidFinCodeAttribute : ValidationAttribute
+    {
+
+        protected override ValidationResult IsValid(object? value,ValidationContext validationContext)
+        {
+            string finCode = value.ToString();
+
+            if (Regex.IsMatch(finCode,@"[A-Z0-9]{7,}")) 
+            {
+                return ValidationResult.Success;
+            }
+            return new ValidationResult("Some Things is Incorrect Please Check Lenght And Other things (Pls Check letters,letters must be capitalized)");
+        }
+    }
+}
